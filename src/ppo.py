454287848353ccff11defcd60450a60997b27780
Jayanth Lalukota <jlalukota@gmail.com>
2025-06-10 10:17:43 -0400

    network merged

--- src/ppo.py
+++ src/ppo.py
@@ -7,9 +7,30 @@ import pygame
 import torch.nn as nn
 from torch.optim import Adam
 from torch.distributions import MultivariateNormal
-from network import FeedForwardNN
+from torch import nn
+import torch.nn.functional as F
+import numpy as np
 
 
+class FeedForwardNN(nn.Module):
+
+    def __init__(self, in_dim, out_dim):
+        super(FeedForwardNN, self).__init__()
+        self.layer1 = nn.Linear(in_dim, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, out_dim)
+
+    def forward(self, obs):
+        # Convert obersation to tensor if it's a numpy arr
+        if isinstance(obs, np.ndarray):
+            obs = torch.tensor(obs, dtype= torch.float32)
+        
+        activation1 = F.relu(self.layer1(obs))
+        activation2 = F.relu(self.layer2(activation1))
+        output = self.layer3(activation2)
+
+        return output
+    
 
 class PPO:
 
